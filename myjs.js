f526afb03db84a4efe90cf5ae08af184696649e4
chueia <512537144@qq.com>
2017-03-21 16:46:46 +0800

    3.21
    添加layer.js
    修复重新游戏后页面数组还在的BUG

--- myjs.js
+++ myjs.js
@@ -1,12 +1,13 @@
 //禁止页面滚动，避免取值错误
- $(document.body).css({
-   "overflow-x":"hidden",
-   "overflow-y":"hidden"
- });
+$(document.body).css({
+    "overflow-x": "hidden",
+    "overflow-y": "hidden"
+});
 //定义全局参数
 var canvas;
 var context;
 var isWhite = true;//先手白棋
+var winner = ''//定义赢家，初始为空
 var chessData = new Array(15);//创建数组记录落子位置
 for (x = 4; x <= 18; x++) {
     chessData[x] = new Array(15);
@@ -24,12 +25,16 @@ function drawRect() {
     canvas = document.getElementById("canvas");//获取canvasID
     context = canvas.getContext("2d");//规定画布维度
     context.fillStyle = '#15aafb'//画布填充颜色
+
+
+
+
     context.fillRect(0, 0, 1024, 768)//绘制画布
 
-        //标题
-        context.fillStyle = "#e4393a";
-        context.font = "40px Arial";
-        context.strokeText('傻淮五子棋',350,50);
+    //标题
+    context.fillStyle = "#e4393a";
+    context.font = "40px Arial";
+    context.strokeText('傻淮五子棋', 350, 50);
 
     //     //再来一局
 
@@ -98,8 +103,8 @@ function isWin(color, x, y) {
     chessData[x][y] = temp;
     lrCount(temp, x, y);//判断左右方向是否胜利
     tbCount(temp, x, y);//判断上下方向是否胜利
-    lt_rbCount(temp,x,y);//判断左上右下方向是否胜利
-    rt_lbCount(temp,x,y);//判断右上左下方向是否胜利
+    lt_rbCount(temp, x, y);//判断左上右下方向是否胜利
+    rt_lbCount(temp, x, y);//判断右上左下方向是否胜利
 }
 function lrCount(temp, x, y) {//左右判定
     var count = 0;//连线总数
@@ -137,59 +142,145 @@ function tbCount(temp, x, y) {//上下判定
     }
     success(temp, --count);
 }
-function lt_rbCount(temp,x,y){//左上，右下判定
+function lt_rbCount(temp, x, y) {//左上，右下判定
     var count = 0;
-    for(var i = x,k = y;i >= 4,k >= 2;i--,k--){//左上判定
-        if(chessData[i][k] == temp){
+    for (var i = x, k = y; i >= 4, k >= 2; i-- , k--) {//左上判定
+        if (chessData[i][k] == temp) {
             ++count;
         } else {
             i = -1;
             k = -1;
         }
     }
-    for(var i = x,k = y;i <= 18,k <= 16;i++,k++){//右下判定
-        
-        if(chessData[i][k] == temp){
+    for (var i = x, k = y; i <= 18, k <= 16; i++ , k++) {//右下判定
+
+        if (chessData[i][k] == temp) {
             ++count;
         } else {
             i = 100;
             k = 100;
         }
     }
-    success(temp,--count);
+    success(temp, --count);
 }
-function rt_lbCount(temp,x,y){//右上，左下判定
+function rt_lbCount(temp, x, y) {//右上，左下判定
     var count = 0;
-    for(var i = x,k = y;i >= 4,k <= 16;i--,k++){//左下判定
-        if(chessData[i][k] == temp){
+    for (var i = x, k = y; i >= 4, k <= 16; i-- , k++) {//左下判定
+        if (chessData[i][k] == temp) {
             ++count;
         } else {
             i = -1;
             k = 100;
         }
     }
-    for(var i = x,k = y;i <= 18,k >= 2;i++,k--){//右上判定
-         if(chessData[i][k] == temp){
+    for (var i = x, k = y; i <= 18, k >= 2; i++ , k--) {//右上判定
+        if (chessData[i][k] == temp) {
             ++count;
         } else {
             i = 100;
             k = -1;
         }
     }
-    success(temp,--count);
+    success(temp, --count);
 }
-function success(temp, count) {
+function success(temp, count) {//判断是否胜利
 
     if (count == 5) {
         if (temp == 1) {
-            alert('白棋赢啦');
+            layer.alert('白棋赢啦');
             window.restart();
+            clearChess();
         } else if (temp == 2) {
-            alert('黑棋赢啦');
+            layer.alert('黑棋赢啦');
             window.restart();
+            clearChess();
         }
 
     } else {
         console.log('no winer');
+    }
+}
+function clearChess() {
+    for (x = 4; x <= 18; x++) {
+        chessData[x] = new Array(15);
+        for (y = 2; y <= 16; y++) {
+            chessData[x][y] = 0;
+        }
+    }
+}
+// layer.alert('墨绿风格，点击确认看深蓝', {
+//   skin: 'layui-layer-molv' //样式类名
+//   ,closeBtn: 0
+// })
+//五子棋AI
+function getScore() {//获取分数
+    var i = new Arraw(2);
+    var score = 0;
+    for (var x = 4; x <= 18; x++) {
+        for (var y = 2; y <= 16; y++) {
+            if (chessData == 0) {//如果当前位置没棋子再判断分数
+                if (judge(x, y) > score) {
+                    score = judge(x, y);
+                    i[0] = x;
+                    i[1] = y;
+                }
+            }
+        }
+    }
+    return i;
+}
+
+function judge(x, y) {//AI算分
+    var a = parseInt(aiLR(x, y, 1)) +
+        parseInt(aiTB(x, y, 1)) +
+        parseInt(aiRT(x, y, 1)) +
+        parseInt(aiLB(x, y, 1));
+    var b = parseInt(aiLR(x, y, 2)) +
+        parseInt(aiTB(x, y, 2)) +
+        parseInt(aiRT(x, y, 2)) +
+        parseInt(aiLB(x, y, 2));
+    var result = a + b;
+    return result;
+}
+
+function aiPlay() {//AI下棋
+    var str = getPosition();
+    aiCheck(str[0], str[1]);//AI检查
+}
+
+function aiCheck() {
+    if (winner != '' && winner != null) {
+        return;
+    }
+    if (isWhite) {
+        color = "white";
+    } else {
+        color = "black"
+    }
+    chess(color, x, y);
+}
+
+function aiLR(x, y, num) {
+    var death = 0;//死路
+    var live = 0;//被堵住
+    var count = 0;//没子
+    var arr = new Array(15);
+    for (var i = 4; i <= 18; i++) {
+        arr[i] = new Array(15);
+        for (var j = 2; j <= 16; j++) {
+            arr[i][j] = chessData[i][j];
+        }
+    }
+    arr[x][y] = num;
+    for (var i = x; i >= 4; i--) {
+        if (arr[i][y] == num) {
+            count++;
+        } else if (arr[i][y] == 0) {
+            live += 1;
+            i = -1;
+        }else{
+            death += 1;
+            i = -1;
+        }
     }
 }
